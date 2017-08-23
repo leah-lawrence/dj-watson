f89eadbe4b2abea5c633d96a92e30b355549be6d
Alexander H Aziz <ahaziz@us.ibm.com>
2017-08-23 11:18:20 -0500

    Updated so js is separte from html.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 (function app() {
   'use strict';
 
-  window.addEventListener('DOMContentLoaded', function appDCL() {
-    // Mean to console.log out, so disabling
-    console.log('Hello World'); // eslint-disable-line no-console
-  });
+  var angularApp = angular.module('myApp', []); // eslint-disable-line no-undef
+  angularApp .controller(
+    'myCtrl',
+    function ctrl($scope, $http) {
+      $scope.cards = 'loading'; // eslint-disable-line no-param-reassign
+      $http.get('http://localhost:3000/db')
+      .then(function gotResponse(response) {
+        $scope.cards = response.data.cards; // eslint-disable-line no-param-reassign
+      })
+      .catch(function errorOnGet(error) {
+        $scope.cards = error; // eslint-disable-line no-param-reassign
+      });
+    }
+  );
 }());
