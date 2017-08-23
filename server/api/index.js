9a169bca159b721b607f32f2ccefa1aa8a9fb216
Alexander H Aziz <ahaziz@us.ibm.com>
2017-08-23 17:02:31 -0500

    Watson post.

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,22 @@
 
 const express = require('express');
 const router = express.Router();
+const watsonApi = require('./watsonApi.js');
+const cards = require('./db.json');
 
 const controller = require('./controller');
 
 router.get('/getLyrics', controller.getLyrics);
+router.get('/postLyricsToWatson', controller.postLyricsToWatson);
+
+router.get('/db', (req, res) => {
+  res.status('200').json(cards);
+});
+
+router.get('/getWatsonData', (req, res) => {
+  watsonApi.getWatsonData((data) => {
+    res.status(200).json(data);
+  });
+});
 
 module.exports = router;
---
+++ server/api/watsonApi.js
@@ -0,0 +1,90 @@
+let DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+let config = require('./../../local.env.json');
+let fs = require('fs');
+const count = 100;
+
+module.exports = {
+  getWatsonData: getWatsonData,
+  postDataToWatson: postDataToWatson
+}
+
+let discovery = new DiscoveryV1({
+  username: config.watson.username,
+  password: config.watson.password,
+  version: config.watson.version,
+  version_date: config.watson.version_date
+});
+
+function postDataToWatson(songs) {
+  return Promise.all(
+    songs.map((song, index) => {
+      return postPromise(song, index);
+    })
+  );
+}
+
+function postPromise(song, index) {
+  return new Promise((resolve, reject) => {
+    console.log(JSON.stringify(song));
+    discovery.addDocument(
+      {
+        environment_id: config.watson.ENVIRONMENT_ID,
+        collection_id: config.watson.COLLECTION_ID,
+        file: {
+          value: Buffer.from(JSON.stringify(song), 'utf8'),
+          options: {
+            filename: 'song'+index+'.json'
+          }
+        }
+      },
+      function(err, data) {
+        if (err) {
+          console.error(err);
+          postPromise(song);
+        } else {
+          console.log(JSON.stringify(data, null, 2));
+          resolve(data);
+        }
+      }
+    )
+  });
+}
+function getWatsonData(callback) {
+
+  let returnValue = [];
+  return discovery.query(
+    {
+      environment_id: config.watson.ENVIRONMENT_ID,
+      collection_id: config.watson.COLLECTION_ID,
+      query_string: '',
+      count: count,
+      offset: count*11
+    },
+    function(error, data) {
+      if (error !== undefined) {
+        console.log('Page: ' + 0 +', Got results  0 to '  + data.results.length + ' out of ' + data.matching_results + ' results.');
+        console.log(data);
+        callback({
+          totalMatches: data.matching_results,
+          results: transformData(data.results)
+        });
+      } else {
+        throw error;
+      }
+    }
+  );
+}
+
+// Helper Functions
+function transformData(watsonData) {
+  let transformedData = watsonData.map(data => {
+    let returnValue = {};
+    returnValue['id'] = data.id;
+    returnValue['album'] = data.album;
+    returnValue['artist'] = data.artist;
+    returnValue['cover'] = data.cover;
+    returnValue['enriched_lyrics']  = data['enriched_lyrics']
+    return returnValue;
+  });
+  return transformedData;
+}
