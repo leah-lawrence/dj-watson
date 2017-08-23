271ad92d02d7e8b0035e504f4dee9032c689b32b
Alexander H Aziz <ahaziz@us.ibm.com>
2017-08-23 17:43:54 -0500

    Remove test data and target real data.

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,9 @@
     'myCtrl',
     function ctrl($scope, $http) {
       $scope.cards = 'loading'; // eslint-disable-line no-param-reassign
-      $http.get('http://localhost:3000/api/db')
+      $http.get('http://localhost:3000/api/getWatsonData')
       .then(function gotResponse(response) {
-        $scope.cards = response.data.cards; // eslint-disable-line no-param-reassign
+        $scope.cards = response.data.results; // eslint-disable-line no-param-reassign
       })
       .catch(function errorOnGet(error) {
         $scope.cards = error; // eslint-disable-line no-param-reassign
