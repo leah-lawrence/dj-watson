5c58a1415bdea32ed81e563149dc883de36008bf
vineetmahadik <vineetmahadik@us.ibm.com>
2017-08-24 10:54:43 -0500

    Test

--- src/js/app.js
+++ src/js/app.js
@@ -7,51 +7,54 @@
 
   angularApp .controller(
     'myCtrl',
-    function ($scope, $http, $location) {
-      $scope.activeControl = null;
-      $scope.filters = [
-        {
-          name: 'anger',
-          selected: false,
-        },
-        {
-          name: 'joy',
-          selected: false,
-        },
-        {
-          name: 'sad',
-          selected: false,
-        },
-      ];
+    [
+      '$scope', '$http', '$location',
+      function ($scope, $http, $location) {
+        $scope.activeControl = null;
+        $scope.filters = [
+          {
+            name: 'anger',
+            selected: false,
+          },
+          {
+            name: 'joy',
+            selected: false,
+          },
+          {
+            name: 'sad',
+            selected: false,
+          },
+        ];
 
-      $scope.cards = 'loading';
-      $http.get($location.absUrl() + '/api/getWatsonData')
-        .then(function gotResponse(response) {
-          $scope.cards = response.data.results;
-        })
-        .catch(function errorOnGet(error) {
-          $scope.cards = error;
-        });
+        $scope.cards = 'loading';
+        $http.get($location.absUrl() + '/api/getWatsonData')
+          .then(function gotResponse(response) {
+            $scope.cards = response.data.results;
+          })
+          .catch(function errorOnGet(error) {
+            $scope.cards = error;
+          });
 
-      $scope.handleCardClick = function handleCardClick() {
-        this.card.selected = true;
+        $scope.handleCardClick = function handleCardClick() {
+          this.card.selected = true;
 
-        // $scope.cards.forEach(function(card) {
-        //   if (card != this.card) {
-        //     card.selected = false;
-        //   }
-        // }.bind(this));
-      };
+          // $scope.cards.forEach(function(card) {
+          //   if (card != this.card) {
+          //     card.selected = false;
+          //   }
+          // }.bind(this));
+        };
 
-      $scope.handleControlClick = function handleControlClick() {
-        this.filter.selected = true;
+        $scope.handleControlClick = function handleControlClick() {
+          this.filter.selected = true;
 
-        // $scope.filters.forEach(function(filter) {
-        //   if (filter != this.filter) {
-        //     filter.selected = false;
-        //   }
-        // }.bind(this))
-      };
-    }
+          // $scope.filters.forEach(function(filter) {
+          //   if (filter != this.filter) {
+          //     filter.selected = false;
+          //   }
+          // }.bind(this))
+        };
+      }
+    ]
   );
 }());
